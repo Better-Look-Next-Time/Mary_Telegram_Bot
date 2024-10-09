464488bffb4a7d0b3d25f03ea5f50847c00a326a
Karter_Kein <MAKCNM_0709@mail.ru>
2024-10-09 15:22:44 +0300

    create bot

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log("Hello via Bun!");
+import { mary } from "@mary/core";
+import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+import { env } from "bun";
+import { WhiteLits } from "./whiteList"; 
+
+const Bot =  new Telegraf(env.BotToken)
+
+Bot.on(message('text'), async (ctx) => {
+   const  chatId = ctx.message.chat.id
+  if (WhiteLits.some(( id ) => id === ctx.message.chat.id )) {
+    console.log('id in whiteList')
+    const  qwestion = ctx.message.text
+    const answer = await mary(qwestion, chatId.toString(), ctx.from.username ?? '', chatId.toString())
+    await ctx.telegram.sendMessage(chatId, answer)
+  } else {
+    console.log('id not whiteList')
+    await ctx.telegram.sendMessage(chatId, 'Прости но я не могу тебе ответить')
+  }
+})
+
+Bot.launch()
