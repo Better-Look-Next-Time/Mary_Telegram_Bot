7edf261317373c7bb1f3608b9784a643ced5fa99
Karter_Kein <MAKCNM_0709@mail.ru>
2024-10-10 20:17:34 +0300

    edit bot

--- index.ts
+++ index.ts
@@ -4,24 +4,31 @@ import { message } from "telegraf/filters";
 import { env } from "bun";
 import { WhiteLits } from "./whiteList"; 
 
-import type { ModelNameType } from "@mary/core/src/models/openai/types";
+import type { MaryConfig } from "@mary/core";
 
 const Bot =  new Telegraf(env.BotToken)
 
-const  config = {
-  thoughtsArray: ['mixtral-8x7b-instruct', 'llama-3.1-8b-instruct'] as ModelNameType[],
-  chapter: 'gpt-4o-mini' as ModelNameType
+const  config :MaryConfig = {
+  thoughtsArray: ['mixtral-8x7b-instruct', 'llama-3.1-8b-instruct'],
+  chapter: 'gpt-4o-mini',
+  creatorImagePrompt: 'llama-3.1-8b-instruct'
 }
 
 
 Bot.on(message('text'), async (ctx) => {
    const  chatId = ctx.message.chat.id
-  if (WhiteLits.some(( id ) => id === ctx.message.chat.id )) {
+   if (WhiteLits.some(( id ) => id === ctx.message.chat.id )) {
     console.log('id in whiteList')
-    const  qwestion = ctx.message.text
-    const mary = new Mary(config, qwestion, chatId.toString(), ctx.from.username ?? '', chatId.toString())
-    const answer = await mary.Request()
-    await ctx.telegram.sendMessage(chatId, answer)
+    const  question = ctx.message.text.toLowerCase()
+    const mary = new Mary(config, question, chatId.toString(), ctx.from.username ?? '', chatId.toString())
+    if (question.includes('нарисуй')) {
+      console.log('рисую изображение')
+      const  answer = await mary.ImageGenerator()
+      await ctx.telegram.sendMessage(chatId, answer)
+    } else {
+      const answer = await mary.Request()
+      await ctx.telegram.sendMessage(chatId, answer)
+    }
   } else {
     console.log('id not whiteList')
     await ctx.telegram.sendMessage(chatId, 'Прости но я не могу тебе ответить')
