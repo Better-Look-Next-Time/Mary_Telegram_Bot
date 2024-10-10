95b59cd2df462dcd35802137db839315ceb6baed
Karter_Kein <MAKCNM_0709@mail.ru>
2024-10-10 15:16:54 +0300

    update  libs

--- index.ts
+++ index.ts
@@ -1,17 +1,26 @@
-import { mary } from "@mary/core";
+import { Mary } from "@mary/core";
 import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
 import { env } from "bun";
 import { WhiteLits } from "./whiteList"; 
 
+import type { ModelNameType } from "@mary/core/src/models/openai/types";
+
 const Bot =  new Telegraf(env.BotToken)
+
+const  config = {
+  thoughtsArray: ['mixtral-8x7b-instruct', 'llama-3.1-8b-instruct'] as ModelNameType[],
+  chapter: 'gpt-4o-mini' as ModelNameType
+}
+
 
 Bot.on(message('text'), async (ctx) => {
    const  chatId = ctx.message.chat.id
   if (WhiteLits.some(( id ) => id === ctx.message.chat.id )) {
     console.log('id in whiteList')
     const  qwestion = ctx.message.text
-    const answer = await mary(qwestion, chatId.toString(), ctx.from.username ?? '', chatId.toString())
+    const mary = new Mary(config, qwestion, chatId.toString(), ctx.from.username ?? '', chatId.toString())
+    const answer = await mary.Request()
     await ctx.telegram.sendMessage(chatId, answer)
   } else {
     console.log('id not whiteList')
